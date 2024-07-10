af75d04586755a482d59c2911bb250861a890729
nicole wachter <nicole.wachter6@gmail.com>
2024-07-10 15:59:47 -0400

    Adds events

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,10 +1,20 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
-  return (
+
+
+function TaskList({ tasks, onDeleteTasks }) {
+   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
-    </div>
+      {tasks.map((task) => (
+        <Task 
+          key={task.text}
+          text={task.text}
+          category={task.category}
+          onDelete={() => onDeleteTasks(task.text)}
+          />
+        ))}
+     </div>
   );
 }
 
